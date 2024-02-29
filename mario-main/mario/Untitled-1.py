d6d13a5aa632dd8f793a4f24a5a0cd0bb6e68328
RomanUhymchuk <141724811+RomanUhymchuk@users.noreply.github.com>
2024-02-29 19:51:51 +0200

    Update Untitled-1.py

--- mario-main/mario/Untitled-1.py
+++ mario-main/mario/Untitled-1.py
@@ -2,29 +2,29 @@ from pygame import *
 
 class GameSprite(sprite.Sprite): 
 
-    def __init__(self, player_image , player_x , player_y, size_x, syze_y, player_speed): 
+    def __init__(self, player_image , player_x , player_y, size_x, size_y, player_speed): 
         sprite.Sprite.__init__(self) 
         self.image = transform.scale(image.load(player_image),(50 , 50))  
         self.speed = player_speed 
         self.rect = self.image.get_rect() 
         self.rect.x = player_x 
         self.rect.y = player_y 
+        self.size_x = size_x
+        self.size_y = size_y
     def reset (self): 
         window.blit(self.image,(self.rect.x , self.rect.y)) 
 
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x :
+            self.rect.x += self.speed
+
+        if keys [K_SPACE] and self.rect.y > -5:      
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
-            self.rect.y += self.speed
-    def update_l(self):
-        keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 80:
-            self.rect.y += self.speed
+  
 win_width = 1200
 win_height = 600
 
@@ -41,8 +41,7 @@ finish = False
 clock = time.Clock()
 FPS = 30
 
-mario = Player('mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_1.png', 100, 470, 200, 200, 150) 
-
+mario = Player('mario-main/mario/Нова папка/smallmariosheet-removebg-preview_1.png', 100, 470, 10, 10, 10) 
 
 
 
@@ -54,10 +53,12 @@ while game:
     for e in event.get():
         if e.type == QUIT:
                 game = False 
-
-
+    if finish != True:
+        window.blit(backk,(0,0))
+        mario.update_r()
 
         mario.reset()
-
+       
     display.update()
     clock.tick(FPS)
+
