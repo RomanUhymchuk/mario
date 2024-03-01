5775d4f6263753227e821d8b1359ab4b4c4d5838
RomanUhymchuk <141724811+RomanUhymchuk@users.noreply.github.com>
2024-03-01 20:05:37 +0200

    Update Untitled-1.py

--- mario-main/mario/Untitled-1.py
+++ mario-main/mario/Untitled-1.py
@@ -16,30 +16,15 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def update(self):
-        global isJump ,jumpCount ,vel 
+
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.x > 5:
+        if keys[K_a] and self.rect.x:
             self.rect.x -= self.speed
         if keys[K_d] and self.rect.x :
             self.rect.x += self.speed
+
+
         
-        if isJump:
-            if jumpCount >= -10:
-                self.rect.y -= (jumpCount * abs(jumpCount)) * 0.5
-                jumpCount -= 1
-            else: 
-                jumpCount = 10
-                isJump = False
-
-        if not(isJump): 
-            if keys[K_a] and self.rect.x > vel:
-                self.rect.x += vel
-
-            if keys[K_d] and self.rect.x > vel:
-                self.rect.x += vel
-
-            if keys[K_SPACE]:
-                isJump = True
 win_width = 1200
 win_height = 600
 
@@ -53,33 +38,50 @@ backk = transform.scale(image.load("1661354146_1-kartinkin-net-p-fon-mario-dlya-
 game = True
 finish = False
 clock = time.Clock()
-FPS = 30
+FPS = 60
 
 mario = Player('mario-main/mario/Нова папка/smallmariosheet-removebg-preview_1.png', 100, 470, 10, 10, 10) 
 
 
-
-
-vel = 5
-isJump = False
-jumpCount = 10
-
+jump_count =8
+jump_height =8
+jumping = False
+x_bg =0 
 run = True
-
 while run:
-    time.delay(100)
+    keys = key.get_pressed()
+    window.blit(backk,(x_bg,0))
+    window.blit(backk,(x_bg-win_height,0))
 
     for e in event.get():
         if e.type == QUIT:
             run = False
-        window.blit(backk,(0,0))
-   
+    if not jumping:
+        if keys[K_SPACE]:
+            jumping = True
+
+    else:
+        if jump_count >= -jump_height:
+            neg = 1
+            if jump_count < 0:
+                neg = -1
+            mario.rect.y -= (jump_count ** 2) * 0.5 * neg
+            jump_count -= 1
+        else:
+            jumping = False
+            jump_count = jump_height
     
 
-        mario.update()
+    mario.update()
 
-        mario.reset()
-       
+    mario.reset()
+    if keys[K_a] :
+        x_bg+=5
+    if keys[K_d] :
+        x_bg-=5
+    if x_bg == -win_width:
+        x_bg=0
+    time.delay(30)
     display.update()
     clock.tick(FPS)
 
