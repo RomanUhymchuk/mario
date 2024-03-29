c883eca0c7a0d15140d04c63adee667a59d749c4
RomanUhymchuk <141724811+RomanUhymchuk@users.noreply.github.com>
2024-03-30 01:08:38 +0200

    Update Untitled-1.py

--- mario-main/mario/Untitled-1.py
+++ mario-main/mario/Untitled-1.py
@@ -3,15 +3,15 @@ from random import randint
 mixer.init() 
 mixer.music.load("mario-main/01. Ground Theme.mp3") 
 mixer.music.play() 
-jump= mixer.Sound("sfx-13 (1).mp3") 
+jump= mixer.Sound("sfx-13.mp3") 
 #make window
 win_width = 1200
 win_height = 600
 window = display.set_mode((1200, 600))
 
 
-player_l = [transform.scale(image.load('mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_11-removebg-preview.png','mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_1.png'), (50, 50)),
-            transform.scale(image.load('mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_13-removebg-preview (1).png','mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_13-removebg-preview (1).png'), (50, 50))]
+player_l = [transform.scale(image.load('mariosheet-removebg-preview_11-removebg-preview.png','mariosheet-removebg-preview_12-removebg-preview (1).png'), (50, 50)),
+            transform.scale(image.load('mariosheet-removebg-preview_13-removebg-preview (12).png'), (50, 50))]
 player_r = [transform.scale(image.load('mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_1.png','mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_13.png'), (50, 50)),
             transform.scale(image.load('mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_13.png'), (50, 50))]
 #make class
@@ -65,7 +65,7 @@ back = transform.scale(image.load("1661354146_1-kartinkin-net-p-fon-mario-dlya-s
 display.set_caption("Mario Game ")
 #class enemy
 mario = Player('mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_1.png', 250, 470, 50, 50, 5) 
-bullet_x = 1000
+bullet_x = 1100
 class Enemy (GameSprite):
     def update(self):
         self.rect.y += self.speed 
@@ -82,37 +82,101 @@ class Enemy(GameSprite):
             self.rect.x = 0 
         if self.rect.x == 0:
             self.kill()
-lakiblock_x=10
-lakiblock = Enemy("wip-new-question-block-animation-made-for-fan-made-mario-v0-5unqv87oj8y91.png", 550, 400, 50,50,0)
+score = 0
+lakiblock_x=0
+lakiblock = Enemy("wip-new-question-block-animation-made-for-fan-made-mario-v0-5unqv87oj8y91.png", 550, 400, 30,30,0)
 bullet = 'mario-main/imgonline-com-ua-Mirror-gZ5dwLoVeP3Q5o-removebg-preview.png'
 monsters = sprite.Group()
 blockcegla = "1692637191_art-oir-mobi-p-mario-kirpichiki-arti-pinterest-2.png"
 mariojump = "mario-main/mario/Нова папка (4)/mariosheet-removebg-preview_7.png"
 font1 = font.Font(None,80)
 text_lose = font1.render("YOU LOSE" ,True ,(255, 0, 0))
-for i in range(1): 
-    monster = Enemy(bullet, bullet_x-100, 470, 50, 50, 5) 
+font2 = font.Font(None,50)
+text_score = font2.render("YOUR SCORE",score ,True ,(0 , 255, 0))
+window.blit(text_score, (1100, 400))
+for i in range(1,8): 
+    monster = Enemy(bullet, 600,1200, 50, 50, 5) 
     monsters.add(monster) 
-    monster = Enemy(bullet, bullet_x + 150 ,360, 50, 50, 5) 
-    monsters.add(monster)            
-# Створення гравця
+         
+#СТВОРЕННЯ БЛОКІВ
 blocks = sprite.Group()
-for i in range(1): 
+for i in range(1):
+    #ОДИНОЧНІ БЛОКИ
+    block = Enemy(blockcegla, 500, 350, 50, 50,0)
+    blocks.add(block)  
+    block = Enemy(blockcegla, 400, 350, 50, 50,0)
+    blocks.add(block)  
+    block = Enemy(blockcegla, 420, 100, 50, 50,0)
+    blocks.add(block) 
+
     block = Enemy(blockcegla, 600, 400, 50, 50,0)
     blocks.add(block)
     block = Enemy(blockcegla, 650, 400, 50, 50,0)
     blocks.add(block) 
     block = Enemy(blockcegla, 700, 400, 50, 50,0)
     blocks.add(block) 
+    block = Enemy(blockcegla, 850, 350, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 900,350, 50, 50,0)
+    blocks.add(block) 
+    block = Enemy(blockcegla, 950, 350, 50, 50,0)
+    blocks.add(block) 
+    block = Enemy(blockcegla, 150, 350, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 200,350, 50, 50,0)
+    blocks.add(block) 
+    block = Enemy(blockcegla, 250, 350, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 250, 180, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 300, 180, 50, 50,0)
+    blocks.add(block) 
+    block = Enemy(blockcegla, 350, 180, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 500, 180, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 550, 180, 50, 50,0)
+    blocks.add(block) 
+    block = Enemy(blockcegla, 600, 180, 50, 50,0)
+    blocks.add(block) 
 
+    block = Enemy(blockcegla, 700, 270, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 750, 270, 50, 50,0)
+    blocks.add(block) 
 
+    block = Enemy(blockcegla, 10, 410, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 50, 410, 50, 50,0)
+    blocks.add(block) 
+
+    block = Enemy(blockcegla, 850, 190, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 900, 190, 50, 50,0)
+    blocks.add(block)
+
+    block = Enemy(blockcegla, 10, 150, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 60, 150, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 110, 150, 50, 50,0)
+    blocks.add(block)
+
+    block = Enemy(blockcegla, 1000, 150, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 1050, 150, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 1100, 150, 50, 50,0)
+    blocks.add(block)
+    block = Enemy(blockcegla, 670, 100, 50, 50,0)
+    blocks.add(block) 
+    block = Enemy(blockcegla, 720, 100, 50, 50,0)
+    blocks.add(block) 
 # Змінні для стрибків
 jump_count = 8
 jump_height = 8
 jumping = False
 
-# Переміщення фону
-x_bg = 0 
 
 x_bg = 0
 y_bg = 0
@@ -124,7 +188,7 @@ while run:
     window.blit(back,(x_bg,0))
     window.blit(back,(x_bg - win_width,0))
     window.blit(back,(x_bg + win_width,0)) # Додана додаткова копія фону для оновлення в іншому напрямку
-
+    window.blit(text_score, (900, 30))
     if sprite.spritecollide(mario, monsters, False):
         #run = False
         window.blit(text_lose, (200, 200))
@@ -160,9 +224,7 @@ while run:
 
     # Переміщення фону
 
-    if keys[K_d]:
-        x_bg -= 15
-        lakiblock_x -= 10
+
 
     monsters.update()
     monsters.draw(window)
